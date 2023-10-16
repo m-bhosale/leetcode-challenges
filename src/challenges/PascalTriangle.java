fc1366f823176a6571e2d590b97aac86b8e66593
m-bhosale <er.mbhosale@gmail.com>
2023-10-16 23:05:13 +0530

    Return the first numRows of Pascal's triangle

---
+++ src/challenges/PascalTriangle.java
@@ -0,0 +1,31 @@
+package challenges;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+        LinkedList<List<Integer>> resultList = new LinkedList();
+        resultList.add(new LinkedList());
+        resultList.getLast().add(1);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> list = new LinkedList();
+            list.add(1);
+            for (int j = 1; j < i; j++) {
+                list.add(resultList.getLast().get(j - 1) + resultList.getLast().get(j));
+            }
+            list.add(1);
+            resultList.add(list);
+        }
+
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        List<List<Integer>> output = generate(numRows);
+    }
+
+}
