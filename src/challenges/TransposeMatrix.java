4188eddbd12a565b04f76731fdaf76bf1323df81
m-bhosale <er.mbhosale@gmail.com>
2023-11-27 22:55:14 +0530

    Return transpose of a matrix

---
+++ src/challenges/TransposeMatrix.java
@@ -0,0 +1,23 @@
+package challenges;
+
+//  Given a 2D integer array matrix, return the transpose of matrix.
+//  The transpose of a matrix is the matrix flipped over its main diagonal,
+//  switching the matrix's row and column indices.
+public class TransposeMatrix {
+
+    public static int[][] transpose(int[][] matrix) {
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] output = transpose(matrix);
+    }
+
+}
